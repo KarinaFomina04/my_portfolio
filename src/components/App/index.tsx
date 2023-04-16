ea7a06ec0b44a8521b707a9f186742146a33f75f
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-04-16 23:13:30 +0200

    Add mu portfolio

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './App.module.scss'
+
+const App: React.FC = () => {
+    return (
+        <div className={s.App}>
+
+        </div>
+    )
+
+}
+
+export default App;
